78d221d7701d947f2ad5d5fd202b6dc4c66f5bae
Jennifer Bolanos <jbolanos707@gmail.com>
2015-05-29 13:07:40 -0700

    find and replace all instances of the word

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
+var findReplace = function(sentence, findWord, replaceWord) {
+  // debugger;
+  var splitString = sentence.split(" ");
+  var re = /\w+/g;
+
+  for(var i=0; i < splitString.length; i++) {
+    if(splitString[i] === findWord) {
+      splitString[i] = replaceWord;
+    }
+  }
+  sentence = sentence.replace(re);
+  sentence = splitString.join(" ");
+  return sentence;
+};
+
+
 var stringSplit = function(sentence) {
   return sentence.split(" ");
 };
@@ -9,7 +25,6 @@ var wordMatch = function(word) {
 };
 
 var findWord = function(sentence, word) {
-  // debugger;
   var splitString = sentence.split(" ");
 
   for(var i=0; i < splitString.length; i++ ) {
--- spec/specs.js
+++ spec/specs.js
@@ -1,3 +1,16 @@
+describe("findReplace", function() {
+  it("will find and replace a single instance of a word", function() {
+    expect(findReplace("cat in the hat", "hat", "roof")).to.equal("cat in the roof");
+  });
+
+  it("will find and replace all instances of the word", function() {
+    expect(findReplace("cat in the cat hat","cat", "dog")).to.equal("dog in the dog hat");
+  });
+
+  
+
+});
+
 describe("stringSplit", function() {
   it("will return the string as an array of words", function() {
     expect(stringSplit("cat in the hat")).to.eql(["cat", "in", "the", "hat"]);
@@ -6,12 +19,12 @@ describe("stringSplit", function() {
 
 describe("wordMatch", function() {
   it("will recognize matching words", function() {
-    expect(wordMatch("hat")).to.equal("hat")
+    expect(wordMatch("hat")).to.equal("hat");
   });
 });
 
 describe("findWord", function() {
   it("will find matching word in the sentence array", function() {
-    expect(findWord("cat in the hat", "hat")).to.eql("hat")
+    expect(findWord("cat in the hat", "hat")).to.eql("hat");
   });
 });
