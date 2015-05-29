f9342097281f3700c54fa1181e5e65df4565c320
Jennifer Bolanos <jbolanos707@gmail.com>
2015-05-29 09:47:37 -0700

    recognize matching words

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 var stringSplit = function(sentence) {
   return sentence.split(" ")
 };
+
+var wordMatch = function(word) {
+  if (word === word) {
+    return word
+  };
+};
+
+// var findWord = function(sentence) {
+//   var sentenceArray = []
+//   sentence.split(" ");
+//     for(var i=0; i < sentence.length; i++) {
+//       if(sentence[i] === " ") {
+//         return i
+//       };
+//     };
+// };
--- spec/specs.js
+++ spec/specs.js
@@ -3,3 +3,15 @@ describe("stringSplit", function() {
     expect(stringSplit("cat in the hat")).to.eql(["cat", "in", "the", "hat"]);
   });
 });
+
+describe("wordMatch", function() {
+  it("will recognize matching words", function() {
+    expect(wordMatch("hat")).to.equal("hat")
+  });
+});
+
+// describe("findWord", function() {
+//   it("will find and return matching word in the sentence array", function() {
+//     expect(findWord(["hat"])).to.eql(["hat"])
+//   });
+// });
